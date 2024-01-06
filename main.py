38977c0571ec91088f5198c3c4986b50f9e571ab
yangshiwei <swearer23@163.com>
2024-01-06 23:23:14 +0800

    simulate sales conversion rate

--- main.py
+++ main.py
@@ -2,17 +2,26 @@ import json
 from simulation.SaleDept import SaleDept
 from simulation.SkuList import SKUList
 
+orders = {}
+
 def gen_SO(saleDept: SaleDept, skuList: SKUList):
   for salesPerson in saleDept.sales_people:
     sku = skuList.get_random_sku()
-    salesPerson.sell(sku)
+    if salesPerson.sell(sku) is not None:
+      if salesPerson.name not in orders:
+        orders[salesPerson.name] = 0
+      orders[salesPerson.name] += 1
 
 def main():
   saleDept = SaleDept()
   saleDept.init_sale_people()
   skuList = SKUList()
   skuList.init()
-  gen_SO(saleDept, skuList)
+  day = 0
+  while day < 100:
+    gen_SO(saleDept, skuList)
+    day += 1
+  print(orders)
 
 if __name__ == '__main__':
   main()
--- simulation/SalesPerson.py
+++ simulation/SalesPerson.py
@@ -1,4 +1,5 @@
 from models.SaleOrder import SaleOrder
+from models.SKU import SKU
 from utils import get_sample
 
 class SalesPerson:
@@ -8,11 +9,23 @@ class SalesPerson:
     self.qty_per_order = kwargs['qty_per_order']
     self.delivery_duration = kwargs['delivery_duration']
 
-  def sell(self, sku):
-    random_qty = get_sample(self.qty_per_order, 0.2)
-    print(f"{self.name} sold {random_qty} of {sku.name}")
+  def sell(self, sku: SKU):
+    if self.is_selling_today() == False:
+      return None
+    sample_order_qty = get_sample(self.qty_per_order, 0.2)
+    print(f"{self.name} sold {sample_order_qty} of {sku.name}")
     saleOrder = SaleOrder(
       sku,
       self.qty_per_order,
       self.delivery_duration
     )
+    return saleOrder
+
+  def is_selling_today(self):
+    prob = self.monthly_order_qty / 30
+    sample_prob = get_sample(prob, 0.5)
+    print(sample_prob)
+    if sample_prob < 0.5:
+      return False
+    else:
+      return True
